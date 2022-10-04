413b1b8161e54be397659ab8099dc0cebddf4bc1
francesco.bruno <>
2022-10-04 15:08:28 +0200

    Remove unused file

--- main.js
+++
@@ -1,6 +0,0 @@
-let windowHash = this.window.location.hash
-console.log(windowHash)
-console.log(this.document.querySelectorAll(`a[href="${windowHash}"`))
-
-let element = this.document.querySelectorAll(`a[href="${windowHash}"`)
-element.classList.add('active')
