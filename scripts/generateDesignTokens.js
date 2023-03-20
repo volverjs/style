33c16a8e3c3ede9997fb80b1e2d6e2ed65e34c35
Alessandro Bellesia <alessandro.bellesia@24consulting.it>
2023-03-20 15:58:14 +0100

    Update to single file storage for figma tokens studio

--- scripts/generateDesignTokens.js
+++ scripts/generateDesignTokens.js
@@ -4,7 +4,7 @@ import postcss from 'postcss'
 import sass from 'sass'
 
 // design tokens
-const designTokens = {}
+const designTokens = { global: {}, dark: {} }
 const icssExports = extractICSS(
 	postcss.parse(sass.compile('./src/export.scss').css),
 ).icssExports
@@ -26,9 +26,12 @@ const exports = Object.keys(icssExports).reduce((accumulator, key) => {
 	return accumulator
 }, {})
 
-designTokens.spacing = Object.keys(exports.spacing).reduce((acc, key) => {
-	acc[`spacing-${key}`] = { value: exports.spacing[key] }
-	return acc
-}, {})
+designTokens.global.spacing = Object.keys(exports.spacing).reduce(
+	(acc, key) => {
+		acc[`spacing-${key}`] = { value: exports.spacing[key] }
+		return acc
+	},
+	{},
+)
 
 fs.writeFileSync('./design-tokens.json', JSON.stringify(designTokens, null, 2))
